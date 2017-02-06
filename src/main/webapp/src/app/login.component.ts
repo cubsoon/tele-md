6d97e73c162a1289a062ea79233b8bbc26417724
nefrey <nefrey@interia.pl>
2017-02-06 20:46:59 +0100

    changed login/logout buttons layout

--- src/main/webapp/src/app/login.component.ts
+++ src/main/webapp/src/app/login.component.ts
@@ -1,33 +1,20 @@
 import { Component, Input } from '@angular/core';
-import { Router }   from '@angular/router';
+import { Auth } from './auth.service';
 
 @Component({
   	selector: 'login-view',
 
   	styles: [`
-  		form {
-  			margin: 50px 0 0 12%;
+  		button {
+  			margin: 50px 0 0 5%;
   		}
   	`],
 
   	template: `
-  		<form name="loginForm" action="/consultations" method="get" ng-controller="LoginController" ng-submit="login(credentials)" novalidate>
-
-  			<label for="username">Login:</label>
-  			<input type="text" id="username" ng-model="credentials.username">
-
-  			<label for="password">Hasło:</label>
-  			<input type="password" id="password" ng-model="credentials.password">
-			
-			<button type="submit" (click)="gotoPage()">Zaloguj</button>
-
-		</form>
+  		<button class="btn btn-primary btn-margin" (click)="auth.login()" *ngIf="!auth.authenticated()">Log In</button>
   	`
 })
 
 export class LoginViewComponent {
-	constructor(private router: Router) {}
-	gotoPage(): void {
-  		this.router.navigate(['/consultations']);
-}
+	constructor(private auth: Auth) {}
 }
